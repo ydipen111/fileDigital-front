d1dd5998e91eb91d75ee6884fce7266d9e21d84c
Your Name <aggressivebanda74@gmail.com>
2024-12-23 08:29:33 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+import { Header } from './components/Header'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Header />
+
+
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MyDocument } from '../features/PdfPage'
+
+const Home = () => {
+  return (
+    <div className='p-5'>
+
+      <MyDocument />
+
+    </div>
+  )
+}
+
+export default Home
